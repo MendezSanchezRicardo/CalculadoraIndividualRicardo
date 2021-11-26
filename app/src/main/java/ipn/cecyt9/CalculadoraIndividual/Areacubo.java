0e26abbcf014dc81aaed39593f08c846f9dba8e7
Calculadora1_Actividad1 <mendez.sanchez.ricardo.antonio@gmail.com>
2021-11-26 03:12:44 -0600

    VFinal

---
+++ app/src/main/java/ipn/cecyt9/CalculadoraIndividual/Areacubo.java
@@ -0,0 +1,44 @@
+package ipn.cecyt9.CalculadoraIndividual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Areacubo extends AppCompatActivity {
+    //MENDEZ SANCHEZ RICARDO ANTONIO 5IM9
+    float arista;
+    float resultado;
+    EditText Aristas, Resultados;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_areacubo);
+
+
+        Aristas = findViewById(R.id.aristas);
+        Resultados = findViewById(R.id.resultado);
+        Resultados.setEnabled(false);
+
+
+        Button btn;
+        btn = findViewById(R.id.calcular);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculo();
+            }
+        });
+    }
+
+    public void calculo() {
+        arista = Float.parseFloat(Aristas.getText().toString());
+        resultado = (arista*arista)*(6);
+        Resultados.setText(String.valueOf(resultado));
+    }
+
+}
